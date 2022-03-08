9a6dda1bea9a6074e32da51ead21ae6f2e49f9d2
GerardoM3 <gerardomonroy13@gmail.com>
2022-03-08 15:14:52 -0600

    Ejemplo de la pág. 3 de la Guía Práctica 1

---
+++ src/Developer/miprimeraaplicacionjava.java
@@ -0,0 +1,29 @@
+
+package Developer;
+
+
+public class miprimeraaplicacionjava {
+    public static void main(String[] args) {
+        
+        String nombreCompleto, direccion, estadoCivil;
+        int edad;
+        double gastosDiarios;
+        char genero;
+        boolean poseeVehiculo;
+        
+        nombreCompleto = "Camila Esperanza Rosales";
+        direccion = "Calle de la armagura desvio la tristeza";
+        estadoCivil = "Soltera";
+        edad = 30;
+        gastosDiarios = 34.45;
+        genero = 'F';
+        poseeVehiculo = false;
+        System.out.println("Nombre completo: " + nombreCompleto);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Estado Civil: " + estadoCivil);
+        System.out.println("Edad: " + edad);
+        System.out.println("Gastos diarios: " + gastosDiarios);
+        System.out.println("Género: " + genero);
+        System.out.println("Posee Vehículo: " + poseeVehiculo);
+    }
+}
