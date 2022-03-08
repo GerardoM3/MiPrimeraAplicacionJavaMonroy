2bdfed549532d9b41be5b426fa36dbc40e8230da
GerardoM3 <gerardomonroy13@gmail.com>
2022-03-08 16:06:24 -0600

    Ejercicio 1 de la Guía Práctica 1

---
+++ src/ejercicios/ValorDeLasMonedas.java
@@ -0,0 +1,12 @@
+
+package ejercicios;
+
+public class ValorDeLasMonedas {
+    public static void main(String[] args) {
+        double monedaInicial, USD, monedaEuro, monedaRU, monedaAust, monedaCan;
+        
+        monedaInicial = 100.00;
+        USD = 1;
+        monedaEuro = 0.70;
+    }
+}
