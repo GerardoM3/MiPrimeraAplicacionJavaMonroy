29bf072f62b79650bd85112e2200d48d78764535
GerardoM3 <gerardomonroy13@gmail.com>
2022-03-08 14:43:19 -0600

    Proyecto Java NetBeans Monroy

---
+++ src/Developer/Variables.java
@@ -0,0 +1,25 @@
+
+package Developer;
+
+
+public class Variables {
+
+    
+    public static void main(String[] args) {
+        sumar();
+        
+    }
+
+    private static void sumar() {
+        int suma, a, b;
+        
+        a = 22;
+        b = 30;
+        
+        suma = a +b;
+        
+        System.out.println("La suma es: " + suma);
+    }
+
+    
+}
